f554a991cc2ef2ab3ec763d99ead6b60a674d15d
Rahulelano <132381959+Rahulelano@users.noreply.github.com>
2025-07-31 16:46:09 +0530

    Update cloudinary.js

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -3,9 +3,9 @@ import 'dotenv/config';
 
 // Configure Cloudinary
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: 'dly16icr8',
+  api_key: '983344653426663',
+  api_secret: 'N_qZKPSDyTeJnjJUz_rygkx3lno',
 });
 
 export default cloudinary;
